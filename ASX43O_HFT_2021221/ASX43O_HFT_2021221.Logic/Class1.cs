e7b95c574ad9d0d5894f9ba3a7d6685d83e07cb0
SirBalatro <trkbalazs@gmail.com>
2021-10-07 18:42:54 +0200

    Hello GIT
    Elso feltoltes
    Ures projectek letrehozva

---
+++ ASX43O_HFT_2021221/ASX43O_HFT_2021221.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ASX43O_HFT_2021221.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ ASX43O_HFT_2021221/ASX43O_HFT_2021221.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ASX43O_HFT_2021221.Models
+{
+    public class Class1
+    {
+    }
+}
