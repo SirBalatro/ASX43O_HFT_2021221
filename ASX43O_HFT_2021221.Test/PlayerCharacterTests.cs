b784b9ff4c9483305a2fa42116f99d314146a482
SirBalatro <trkbalazs@gmail.com>
2021-11-21 12:12:03 +0100

    Preparing testing enviroment

---
+++ ASX43O_HFT_2021221.Test/PlayerCharacterTests.cs
@@ -0,0 +1,28 @@
+﻿using ASX43O_HFT_2021221.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASX43O_HFT_2021221.Test
+{
+    [TestFixture]
+    public class PlayerCharacterTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void PlayerInitializes()
+        {
+            var p = new PlayerCharacter();
+
+            Assert.That(p.CharacterLevel.Equals(0));
+        }
+    }
+}
